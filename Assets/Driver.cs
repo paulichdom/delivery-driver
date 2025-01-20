351be02b99109ceaad4540ea5d41c679c861ff93
paulichdom <paulic.domagoj@gmail.com>
2025-01-20 17:48:35 +0100

    impl boosts and bumps

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] int steeringAngle = 300;
-    [SerializeField] int acceleration = 20;
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] int steerSpeed = 300;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
     
     private void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steeringAngle * Time.deltaTime;
-        float accelerationAmount = Input.GetAxis("Vertical") * acceleration * Time.deltaTime;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float accelerationAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, accelerationAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(("Boost")))
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
