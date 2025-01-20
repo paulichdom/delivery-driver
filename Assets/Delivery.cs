152f201f8b3b975913297fb219733bbbc92d1ea2
paulichdom <paulic.domagoj@gmail.com>
2025-01-20 17:29:38 +0100

    use get component to manipulate sprite renderer

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    
     private bool _hasPackage;
     [SerializeField] float destroyDelay = 0.5f;
+    
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision detected!");
@@ -15,6 +26,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             _hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
         
@@ -22,6 +34,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package delivered!");
             _hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 } 
